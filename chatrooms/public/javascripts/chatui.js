f53155838cc5e0307be023a08083f7d8a94249b3
EvilNOP <evilnops@gmail.com>
2017-10-15 17:54:20 +0800

    Added `processUserInput` function

--- chatrooms/public/javascripts/chatui.js
+++ chatrooms/public/javascripts/chatui.js
@@ -5,3 +5,22 @@ function divEscapedContentElement(message) {
 function divSystemContentElement(message) {
   return $('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp, socket) {
+  const message = $('#send-message').val();
+  let systemMessage;
+
+  if (message.charAt(0) == '/') {
+    systemMessage = chatApp.processCommand(message);
+
+    if (systemMessage) {
+      $('#messages').append(divSystemContentElement(systemMessage));
+    }
+  } else {
+    chatApp.sendMessage($('#room').text(), message); 
+    $('#messages').append(divEscapedContentElement(message)); 
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+
+  $('#send-message').val('');
+}
