d39a71108046184ca855cfb49ab415f25ae79b8a
EvilNOP <evilnops@gmail.com>
2017-10-22 22:22:36 +0800

    Created `server.js`

---
+++ todolist/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const items = [];
+
+const server = http.createServer((req, res) => {
+  if ('/' == req.url) {
+    switch (req.method) {
+      case 'GET':
+        show(res);
+        break;
+      case 'POST':
+        add(req, res);
+        break;
+      default:
+        badRequest(res);
+    }
+  } else {
+    notFound(res);
+  }
+});
+
+server.listen(3000);
