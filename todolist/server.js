b3e36415c372f95441a22786ba80c01b7cfd3b96
EvilNOP <evilnops@gmail.com>
2017-10-23 22:57:44 +0800

    Added to-do list form and item list

--- todolist/server.js
+++ todolist/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const qs = require('querystring');
 
 const items = [];
 
@@ -20,3 +21,49 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+function show(response) {
+  const html = '<html><head><title>Todo List</title></head><body>'
+    + '<h1>Todo List</h1>'
+    + '<ul>'
+    + items.map(function(item){
+        return '<li>' + item + '</li>'
+      }).join('')
+    + '</ul>'
+    + '<form method="post" action="/">'
+    + '<p><input type="text" name="item" /></p>'
+    + '<p><input type="submit" value="Add Item" /></p>'
+    + '</form></body></html>';
+
+  response.setHeader('Content-Type', 'text/html');
+  response.setHeader('Content-Length', Buffer.byteLength(html));
+  response.end(html);
+}
+
+function notFound(response) {
+  response.statusCode = 404;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Not Found');
+}
+
+function badRequest(response) {
+  response.statusCode = 400;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Bad request');
+}
+
+function add(request, response) {
+  let body = '';
+
+  request.setEncoding('utf8');
+  request.on('data', chunk => {
+    body += chunk;
+  });
+  request.on('end', () => {
+    const obj = qs.query(body);
+
+    items.push(obj.item);
+    
+    show(response);
+  });
+}
